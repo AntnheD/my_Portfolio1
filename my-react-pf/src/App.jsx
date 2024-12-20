a23e31892cce62f2c0eef24e15ae1a73f5bdf33c
AntnheD <antnhe.debebe@aait.edu.et>
2024-12-20 09:10:17 +0300

    some update

--- my-react-pf/src/App.jsx
+++ my-react-pf/src/App.jsx
@@ -1,20 +1,13 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About'; // Example component
-import Header from './components/Header';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+// import Header from "./components/Header";
 import './App.css';
+
 function App() {
   return (
-    <Router>
-      <div>
-      <Header/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} /> {/* Example route */}
-        </Routes>
-      </div>
-    </Router>
+    <div className='App'>
+     <div><Home /></div>
+    </div>
   );
 }
 
